f5a1205807c55efc835beeef608e0a4ea9df72b1
Brian Gardner <brngardner@gmail.com>
2017-08-09 19:40:49 -0400

    Add CompletedListFragment
    * This will display the completed tasks

---
+++ app/src/main/java/com/bignerdranch/android/projecttracker/controller/CompletedListFragment.kt
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.projecttracker.controller
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bignerdranch.android.projecttracker.R
+import com.bignerdranch.android.projecttracker.databinding.FragmentListBinding
+
+class CompletedListFragment: Fragment() {
+
+    lateinit var binding: FragmentListBinding
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+
+        return binding.root
+    }
+}
