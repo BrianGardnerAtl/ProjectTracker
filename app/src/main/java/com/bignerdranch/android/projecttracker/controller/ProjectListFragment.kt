7710c5fca0c425ffdded86ef98dbf8dcac971bfd
Brian Gardner <brngardner@gmail.com>
2017-08-09 19:38:15 -0400

    Add ProjectListFragment
    * Rename fragment_task_list to fragment_list so it can be used in
    multiple fragment classes

--- app/src/main/java/com/bignerdranch/android/projecttracker/controller/ProjectListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/projecttracker/controller/ProjectListFragment.kt
@@ -1,15 +1,17 @@
 package com.bignerdranch.android.projecttracker.controller
 
+
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import com.bignerdranch.android.projecttracker.R
 import com.bignerdranch.android.projecttracker.databinding.FragmentListBinding
 
-class TaskListFragment : Fragment() {
+class ProjectListFragment : Fragment() {
 
     lateinit var binding: FragmentListBinding
 
