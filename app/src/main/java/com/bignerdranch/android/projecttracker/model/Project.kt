be37ae4487bd69a63d33f812f274a2bb649b1a1e
Brian Gardner <brngardner@gmail.com>
2017-08-09 20:59:29 -0400

    Make Project class an Entity

--- app/src/main/java/com/bignerdranch/android/projecttracker/model/Project.kt
+++ app/src/main/java/com/bignerdranch/android/projecttracker/model/Project.kt
@@ -1,5 +1,12 @@
 package com.bignerdranch.android.projecttracker.model
 
-class Project(var name: String, var importance: Importance) {
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 
+@Entity
+class Project {
+    @PrimaryKey(autoGenerate = true)
+    var id = 0
+    var name = ""
+    var importance = Importance.NORMAL
 }
