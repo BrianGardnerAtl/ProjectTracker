404324eaca3e34e20f210ea7224b953e35494a05
Brian Gardner <brngardner@gmail.com>
2017-08-10 16:04:23 -0400

    Ignore values until type connectors added

--- app/src/main/java/com/bignerdranch/android/projecttracker/model/Project.kt
+++ app/src/main/java/com/bignerdranch/android/projecttracker/model/Project.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.projecttracker.model
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.PrimaryKey
 
 @Entity
@@ -8,5 +9,6 @@ class Project {
     @PrimaryKey(autoGenerate = true)
     var id = 0
     var name = ""
+    @Ignore
     var importance = Importance.NORMAL
 }
