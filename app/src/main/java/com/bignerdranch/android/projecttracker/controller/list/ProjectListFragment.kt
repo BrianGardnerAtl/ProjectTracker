bbf3c31105f885159ef2e67d49c5cba8fc8a7db2
Brian Gardner <brngardner@gmail.com>
2017-08-10 18:09:38 -0400

    Hook up FAB in project list to open create screen

--- app/src/main/java/com/bignerdranch/android/projecttracker/controller/list/ProjectListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/projecttracker/controller/list/ProjectListFragment.kt
@@ -1,14 +1,15 @@
 package com.bignerdranch.android.projecttracker.controller.list
 
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.bignerdranch.android.projecttracker.R
+import com.bignerdranch.android.projecttracker.controller.create.project.CreateProjectActivity
 import com.bignerdranch.android.projecttracker.databinding.FragmentListBinding
 
 class ProjectListFragment : Fragment() {
@@ -22,7 +23,13 @@ class ProjectListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+        binding.addItem.setOnClickListener(addItemListener)
 
         return binding.root
+    }
+
+    private var addItemListener = View.OnClickListener {
+        val intent = Intent(activity, CreateProjectActivity::class.java)
+        startActivity(intent)
     }
 }
