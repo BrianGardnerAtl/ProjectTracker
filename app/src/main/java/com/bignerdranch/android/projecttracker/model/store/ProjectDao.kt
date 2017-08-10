9d84d7d0ed0ed54700bfedf77400135bd014e86c
Brian Gardner <brngardner@gmail.com>
2017-08-09 21:02:38 -0400

    Add ProjectDao class
    * Add Insert, Update, Delete, and Query methods

---
+++ app/src/main/java/com/bignerdranch/android/projecttracker/model/store/ProjectDao.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.projecttracker.model.store
+
+import android.arch.persistence.room.*
+import com.bignerdranch.android.projecttracker.model.Project
+import io.reactivex.Flowable
+
+@Dao
+interface ProjectDao {
+    @Insert
+    fun insertProject(project: Project)
+
+    @Update
+    fun updateProject(project: Project)
+
+    @Delete
+    fun deleteProject(project: Project)
+
+    @Query("SELECT * FROM project")
+    fun getAllProjects(): Flowable<Project>
+}
