e52ba12498bf25b3c8e369d10acea9a82a435717
Brian Gardner <brngardner@gmail.com>
2017-08-09 21:33:19 -0400

    Add injects method to component

--- app/src/main/java/com/bignerdranch/android/projecttracker/inject/AppComponent.kt
+++ app/src/main/java/com/bignerdranch/android/projecttracker/inject/AppComponent.kt
@@ -1,7 +1,13 @@
 package com.bignerdranch.android.projecttracker.inject
 
+import com.bignerdranch.android.projecttracker.controller.CompletedListFragment
+import com.bignerdranch.android.projecttracker.controller.ProjectListFragment
+import com.bignerdranch.android.projecttracker.controller.TaskListFragment
 import dagger.Component
 
 @Component(modules = arrayOf(AppModule::class, DatabaseModule::class))
 interface AppComponent {
+    fun inject(fragment: ProjectListFragment)
+    fun inject(fragment: CompletedListFragment)
+    fun inject(fragment: TaskListFragment)
 }
