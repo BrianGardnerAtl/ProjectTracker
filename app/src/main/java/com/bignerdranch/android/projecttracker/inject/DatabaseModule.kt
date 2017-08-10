571d7be5bebcc5f2d3dc9286db5aba202143088e
Brian Gardner <brngardner@gmail.com>
2017-08-10 16:03:32 -0400

    Provide ProjectDao in database module
    * Added missing provides for the database name

--- app/src/main/java/com/bignerdranch/android/projecttracker/inject/DatabaseModule.kt
+++ app/src/main/java/com/bignerdranch/android/projecttracker/inject/DatabaseModule.kt
@@ -3,13 +3,28 @@ package com.bignerdranch.android.projecttracker.inject
 import android.arch.persistence.room.Room
 import android.content.Context
 import com.bignerdranch.android.projecttracker.model.store.AppDatabase
+import com.bignerdranch.android.projecttracker.model.store.ProjectDao
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class DatabaseModule {
     @Provides
-    fun providesDatabase(context: Context, databaseName: String): AppDatabase {
+    fun providesProjectDao(database: AppDatabase): ProjectDao {
+        return database.projectDao()
+    }
+
+    // Implementation details
+    @Provides
+    fun providesDatabase(context: Context, @Named("database_file") databaseName: String):
+            AppDatabase {
         return Room.databaseBuilder(context, AppDatabase::class.java, databaseName).build()
+    }
+
+    @Provides
+    @Named("database_file")
+    fun providesDatabaseFileName(): String {
+        return "project_tracker_database"
     }
 }
